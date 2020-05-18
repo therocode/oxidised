fb9cb723d57b08a6f2750972c9fa656505c6d1b3
HeXaGoN <traviswg9@gmail.com>
2020-05-17 22:55:00 -0500

    Submit task 1a code

--- hexagon/task1a/src/main.rs
+++ hexagon/task1a/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut mass_str = String::new();
+    
+    print!("Enter mass of module: ");
+    io::stdout().flush().unwrap();
+
+    io::stdin().read_line(&mut mass_str).unwrap(); 
+    let mass: f64 = mass_str.trim().parse().unwrap();
+
+    let fuel = (mass/3.0).floor() - 2.0;
+    println!("Fuel required for launch: {}", fuel as i64);
 }
