c0b986131ba5d386787f228fb01fd584bdaeb124
HeXaGoN <traviswg9@gmail.com>
2020-05-19 18:50:12 -0500

    Handle task 1a and 1b

--- hexagon/task1/src/main.rs
+++ hexagon/task1/src/main.rs
@@ -4,32 +4,39 @@ use std::{
     error
 };
 
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let file = File::open("modules.txt")?;
+    let reader = BufReader::new(file);
+
+    let (fuel, total_fuel) = {
+        let mut sum = (0.0, 0.0);
+        for line in reader.lines() {
+            let mass = line?.trim().parse::<f64>()?;
+            let fuel = fuel_required(mass);
+            sum.0 += fuel;
+            sum.1 += additional_fuel(fuel);
+        }
+        sum
+    };
+
+    println!("Fuel required for launching modules: {}", fuel);
+    println!("Total fuel required: {}", total_fuel);
+
+    Ok(())
+}
+
 fn fuel_required(mass: f64) -> f64
 {
     return f64::max((mass/3.0).floor()-2.0, 0.0);
 }
 
-fn fuel_required_adjusted(mass: f64) -> f64
+fn additional_fuel(mass: f64) -> f64
 {
-    let mut fuel = fuel_required(mass);
+    let mut fuel = mass;
     let mut inc = fuel;
     while inc > 0.0 {
         inc = fuel_required(inc);
         fuel += inc;
     }
     return fuel;
-}
-
-fn main() -> Result<(), Box<dyn error::Error>> {
-    let file = File::open("modules.txt").unwrap();
-    let reader = BufReader::new(file);
-
-    let mut fuel = 0.0;
-    for line in reader.lines() {
-        fuel += fuel_required_adjusted(line?.trim().parse::<f64>()?);
-    }
-
-    println!("Fuel required for launching modules: {}", fuel);
-
-    Ok(())
 }
