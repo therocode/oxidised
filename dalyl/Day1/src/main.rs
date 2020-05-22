3d30332be3dc1675b35eec0fa3c38ba9c4c403dd
dalyl-zero <alidalyl@yahoo.fr>
2020-05-22 02:38:12 +0100

    Finish Day1 !

--- dalyl/Day1/src/main.rs
+++ dalyl/Day1/src/main.rs
@@ -1,17 +1,28 @@
-use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
+use std::fs::File;
 
 fn main() -> io::Result<()> {
     let file = File::open("input.txt").expect("Error reading input from file");
     let reader = BufReader::new(file);
 
-    let mut fuel: u32 = 0;
+    let mut total_fuel = 0;
     for line in reader.lines() {
-        let mass = line?.parse::<u32>().unwrap();
-        fuel += ((mass / 3) - 2);
+        let mass = line?.parse::<i32>().unwrap();
+        total_fuel += additional_fuel(mass);
     }
 
-    println!("{}", fuel);
+    println!("{}", total_fuel);
 
     Ok(())
+}
+
+fn additional_fuel(mut mass: i32) -> i32 {
+    let mut fuel = 0;
+    while mass > 0 {
+        mass = (mass / 3) - 2;
+        if mass > 0 {
+            fuel += mass;
+        }
+    }
+    return fuel;
 }
